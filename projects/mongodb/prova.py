3a84c6c4245136a637e1a1529927d95beb715663
Chuck <chuck@Chuck-pc.homenet.telecomitalia.it>
2016-05-24 18:14:09 +0200

    Secondo commit mongodb

--- projects/mongodb/prova.py
+++ projects/mongodb/prova.py
@@ -1,11 +1,15 @@
 from pymongo import *
+import json
+from caricaDati import *
 from query import *
 
 client = MongoClient("localhost", 27017)
 db = client.test
 
-queryInserisciSalute(db)
-queryPrendiSalute(db)
+with open('dataset.json') as data_file:
+    data = json.load(data_file)
+
+popolaTutto(data,db)
 
 print "\nfine programma"
 
--- projects/mongodb/query.py
+++ projects/mongodb/query.py
@@ -1,145 +1,282 @@
-def queryInserisciPaziente(db):
+#PAZIENTE
+def queryInserisciPaziente(db,id,cognome,nome,data,indirizzo,telefono,email):
     collection = db.Paziente
     collection.insert({
-        "id" : 0,
-		"cognome" : "gangemi",
-		"nome" : "salvatore",
-		"dataDiNascita": "05/06/1994",
-		"indirizzo" : "via gangemi",
-		"telefono" : "450445",
-		"email" : "email@email.it"
+        "id" : id,
+		"cognome" : nome,
+		"nome" : cognome,
+		"dataDiNascita": data,
+		"indirizzo" : indirizzo,
+		"telefono" : telefono,
+		"email" : email,
    })
 
 def queryPrendiPazienti(db):
     collection = db.Paziente
     print "\n INIZIO QUERY \n"
-    pazienti = collection.find({'id': "0"})
+    pazienti = collection.find({})
     for paz in pazienti:
         print(paz)
     print "\n FINE QUERY \n"
 
+
+#INSTALLAZIONE
+
+def queryInserisciInstallazione(db,id,dove,quando,id_paziente,id_device):
+    collection = db.Install
+    collection.insert({
+        "id" : id,
+		"dove" : dove,
+		"quando" : quando,
+		"id_paziente" : id_paziente,
+		"id_device" : id_device,
+   })
+
 def queryPrendiInstallazione(db):
     collection = db.Install
     print "\n INIZIO QUERY \n"
-    installazioni = collection.find({'id': "0"})
+    installazioni = collection.find({})
     for ins in installazioni:
         print(ins)
     print "\n FINE QUERY \n"
 
-def queryInserisciInstallazione(db):
-    collection = db.Install
-    collection.insert({
-        "id" : 0,
-		"dove" : "messina",
-		"quando" : "24/05/2016",
-		"id_paziente" : "0",
-		"id_device" : "0",
-   })
 
-def queryInserisciDevice(db):
+#DEVICE
+
+def queryInserisciDevice(db,id,produttore,modello):
     collection = db.Device
     collection.insert({
-        "id" : 0,
-		"produttore" : "samsung",
-		"modello" : "galaxy s2",
+        "id" : id,
+		"produttore" : produttore,
+		"modello" : modello,
     })
 
 def queryPrendiDevice(db):
     collection = db.Device
     print "\n INIZIO QUERY \n"
-    devices = collection.find({'id': "0"})
+    devices = collection.find({})
     for dev in devices:
         print(dev)
     print "\n FINE QUERY \n"
 
-def queryInserisciParametri(db):
+
+#PARAMETRI
+
+def queryInserisciParametri(db,id,descrizione,frequenza):
     collection = db.Parametri
     collection.insert({
-        "id" : 0,
-		"descrizione" : "descrizione",
-		"frequenza" : "frequenza",
+        "id" : id,
+		"descrizione" : descrizione,
+		"frequenza" : frequenza,
     })
 
 def queryPrendiParametri(db):
     collection = db.Parametri
     print "\n INIZIO QUERY \n"
-    devices = collection.find({'id': "0"})
+    devices = collection.find({})
     for dev in devices:
         print(dev)
     print "\n FINE QUERY \n"
 
 
 
+#MISURAZIONI
+
+def queryInserisciMisurazioni(db,id,id_device,id_parametro):
+    collection = db.Misurazioni
+    collection.insert({
+        "id" : id,
+		"id_device" : id_device,
+		"id_parametro" : id_parametro,
+    })
+
+def queryPrendiMisurazioni(db):
+    collection = db.Misurazioni
+    print "\n INIZIO QUERY \n"
+    devices = collection.find({})
+    for dev in devices:
+        print(dev)
+    print "\n FINE QUERY \n"
 
 
+#OSSERVAZIONI
 
-
-def queryInserisciOsservazioni(db):
+def queryInserisciOsservazioni(db,id,timestamp,value,uom):
     collection = db.Osservazioni
     collection.insert({
-        "id" : "0",
-		"timestamp" : "descrizione",
-		"value" : "frequenza",
-		"uom" : "uom",
-		"id_parametro" : "0",
+        "id" : id,
+		"timestamp" : timestamp,
+		"value" : value,
+		"uom" : uom,
     })
 
 def queryPrendiOsservazioni(db):
     collection = db.Osservazioni
     print "\n INIZIO QUERY \n"
-    osservazioni = collection.find({'id': "0"})
+    osservazioni = collection.find({})
     for oss in osservazioni:
         print(oss)
     print "\n FINE QUERY \n"
 
 
 
+#EFFETTO
 
-
-
-
-def queryInserisciEffetto(db):
+def queryInserisciEffetto(db,id,id_osservazione,id_salute):
     collection = db.Effetto
     collection.insert({
-        "id" : "0",
-		"id_osservazione" : "descrizione",
-		"id_salute" : "frequenza",
+        "id" : id,
+		"id_osservazione" : id_osservazione,
+		"id_salute" : id_salute,
     })
 
 def queryPrendiEffetto(db):
     collection = db.Effetto
     print "\n INIZIO QUERY \n"
-    salute = collection.find({'id': "0"})
+    salute = collection.find({})
     for sal in salute:
         print(sal)
     print "\n FINE QUERY \n"
 
 
 
+#SALUTE
 
-
-
-
-def queryInserisciSalute(db):
+def queryInserisciSalute(db,id,timestamp,malattia,grado_malattia):
     collection = db.Salute
     collection.insert({
-        "id" : "0",
-		"timestamp" : "descrizione",
-		"malattia" : "frequenza",
-		"grado_malattia" : "frequenza",
-		"id_paziente" : "0"
+        "id" : id,
+		"timestamp" : timestamp,
+		"malattia" : malattia,
+		"grado_malattia" : grado_malattia,
     })
 
 def queryPrendiSalute(db):
     collection = db.Salute
     print "\n INIZIO QUERY \n"
-    salute = collection.find({'id': "0"})
+    salute = collection.find({})
     for sal in salute:
         print(sal)
     print "\n FINE QUERY \n"
 
 
 
+#SETTARE
+
+def queryInserisciSettare(db,id,id_terapia,id_salute):
+    collection = db.Settare
+    collection.insert({
+        "id" : id,
+		"id_terapia" : id_terapia,
+		"id_salute" : id_salute,
+    })
+
+def queryPrendiSettare(db):
+    collection = db.Settare
+    print "\n INIZIO QUERY \n"
+    salute = collection.find({})
+    for sal in salute:
+        print(sal)
+    print "\n FINE QUERY \n"
 
 
 
+#TERAPIA
+
+def queryInserisciTerapia(db,id,tempo_avvio,durata,medicina,posologia):
+    collection = db.Terapia
+    collection.insert({
+        "id" : id,
+		"tempo_avvio" : tempo_avvio,
+		"durata" : durata,
+		"medicina" : medicina,
+		"posologia" : posologia,
+    })
+
+def queryPrendiTerapia(db):
+    collection = db.Terapia
+    print "\n INIZIO QUERY \n"
+    salute = collection.find({})
+    for sal in salute:
+        print(sal)
+    print "\n FINE QUERY \n"
+
+
+
+#VALUTARE
+
+def queryInserisciValutare(db,id,id_dottore,id_salute):
+    collection = db.Valutare
+    collection.insert({
+        "id" : id,
+		"id_dottore" : id_dottore,
+		"id_salute" : id_salute,
+    })
+
+def queryPrendiValutare(db):
+    collection = db.Valutare
+    print "\n INIZIO QUERY \n"
+    salute = collection.find({})
+    for sal in salute:
+        print(sal)
+    print "\n FINE QUERY \n"
+
+
+
+#DOTTORE
+
+def queryInserisciDottore(db,id,nome,cognome):
+    collection = db.Dottore
+    collection.insert({
+        "id" : id,
+		"nome" : nome,
+		"cognome" : cognome,
+    })
+
+def queryPrendiDottore(db):
+    collection = db.Dottore
+    print "\n INIZIO QUERY \n"
+    salute = collection.find({})
+    for sal in salute:
+        print(sal)
+    print "\n FINE QUERY \n"
+
+
+
+#MONITORAGGIO
+
+def queryInserisciMonitoraggio(db,id,id_osservazione,id_parametro):
+    collection = db.Monitoraggio
+    collection.insert({
+        "id" : id,
+		"id_osservazione" : id_osservazione,
+		"id_parametro" : id_parametro,
+    })
+
+def queryPrendiMonitoraggio(db):
+    collection = db.Monitoraggio
+    print "\n INIZIO QUERY \n"
+    salute = collection.find({})
+    for sal in salute:
+        print(sal)
+    print "\n FINE QUERY \n"
+
+
+
+#RELATIVO
+
+def queryInserisciRelativo(db,id,id_salute,id_paziente):
+    collection = db.Relativo
+    collection.insert({
+        "id" : id,
+		"id_salute" : id_salute,
+		"id_paziente" : id_paziente,
+    })
+
+def queryPrendiRelativo(db):
+    collection = db.Relativo
+    print "\n INIZIO QUERY \n"
+    salute = collection.find({})
+    for sal in salute:
+        print(sal)
+    print "\n FINE QUERY \n"
